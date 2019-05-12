e5f5b818edfcaf97e3aaa9d9a121d79afec5c068
18sby_2 <1059101993@qq.com>
2019-05-12 17:33:21 +0800

    konva_share

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Axios from 'axios'
+// canvas库
+import VueKonva from 'vue-konva'
+// vant - mobile ui
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+Vue.use(VueKonva)
 
 Vue.prototype.$axios = Axios
 Vue.prototype.HOST = '/baidu_music_api'
